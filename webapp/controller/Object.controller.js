b5ae3e7eb0a2c7f8a82b194df16d1f02a90b0400
Dishant Sambyal <sambyaldishant@gmail.com>
2025-05-21 11:07:51 +0000

    ss

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
 
-  return Controller.extend("sls.jbd.subbd.sljbdttcsubbduem.controller.Objects", {
+  return Controller.extend("sls.jbd.subbd.sljbdttcsubbduem.controller.Object", {
     onInit: function () {},
   });
 });
